2fb2b0e0353b47f8fce25526162ade8166208b9f
Amit Singh <abcdamit44@gmail.com>
2022-03-28 12:26:00 +0530

    Fix

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="My React App">My React App</h1>
-      <p data-ns-test="My React App">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore accusantium amet optio facere molestias tempora vero, quasi laudantium, veniam sit sint velit repellat aut impedit, perferendis reprehenderit quis placeat.</p>
+      <h1 data-ns-test="project-name">My React App</h1>
+      <p data-ns-test="project-name">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore accusantium amet optio facere molestias tempora vero, quasi laudantium, veniam sit sint velit repellat aut impedit, perferendis reprehenderit quis placeat.</p>
     </div>
   )
 }
