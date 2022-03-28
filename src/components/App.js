699be61911935b7133307cf022deb5c00316ada5
Amit Singh <abcdamit44@gmail.com>
2022-03-28 12:29:59 +0530

    Fix

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div id="main">
       <h1 data-ns-test="project-name">My React App</h1>
-      <p data-ns-test="project-name">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore accusantium amet optio facere molestias tempora vero, quasi laudantium, veniam sit sint velit repellat aut impedit, perferendis reprehenderit quis placeat.</p>
+      <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore accusantium amet optio facere molestias tempora vero, quasi laudantium, veniam sit sint velit repellat aut impedit, perferendis reprehenderit quis placeat.</p>
     </div>
   )
 }
