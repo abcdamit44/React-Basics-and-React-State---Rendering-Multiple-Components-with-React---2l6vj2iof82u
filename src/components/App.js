46d872d7e0c0bb1f53b8fd84be31faff6bf37abd
Amit Singh <abcdamit44@gmail.com>
2022-03-28 12:18:47 +0530

    Fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="My React App">My React App</h1>
+      <p data-ns-test="My React App">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore accusantium amet optio facere molestias tempora vero, quasi laudantium, veniam sit sint velit repellat aut impedit, perferendis reprehenderit quis placeat.</p>
+    </div>
   )
 }
 
